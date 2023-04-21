c026dcff2e9afc4f6cd5b8407f86253d2cd7fafd
krisikostadinov <krisi.199898@gmail.com>
2023-04-22 02:48:44 +0300

    feature: global error handler

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const { errorHandler } = require("./config/middlewares");
+
 const app = express();
 
 // common middlewares
@@ -13,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // routes
 const { initialRouter } = require("./routers");
 app.use("/", initialRouter);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log("Server started on port: " + process.env.PORT);
