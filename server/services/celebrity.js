4c8c30c8c54137dbcfd7fbd0ea3a4ddc7508ca3c
krisikostadinov <krisi.199898@gmail.com>
2023-04-22 03:01:30 +0300

    feature: update celebrity

--- server/services/celebrity.js
+++ server/services/celebrity.js
@@ -13,6 +13,18 @@ const post = {
         
         return promise;
     },
+    update: (id, data) => {
+        const sql = `UPDATE celebrities SET famous_name='${data.famous_name}',image_url='${data.image_url}' WHERE id = ${id}`;
+
+        const promise = new Promise((resolve, reject) => {
+            connection.query(sql, (err, result) => {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+        
+        return promise;
+    },
     createBulk: (data) => {
         const query = "INSERT INTO celebrities (famous_name, image_url) VALUES ?";
         const values = [];
