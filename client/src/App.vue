dcda7e580d4800b92252d6a7a993051903ff7ac6
krisikostadinov <krisi.199898@gmail.com>
2023-04-22 03:14:02 +0300

    feature: created bottom navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="text-4xl text-center my-10">Познай извесната личност</h1>
+  <bottom-navbar />
 </template>
 
 <script>
---
+++ client/src/components/BottomNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed bottom-0 w-full border-t border-primary h-20">
+    <div
+      class="container mx-auto text-xl flex items-center gap-5 w-full h-full"
+    >
+      <button class="primary-btn" title="Верни отговири">10</button>
+      <button class="primary-btn" title="Грешени отговири">5</button>
+      <button class="primary-btn" title="Общо изпратени отговори">10</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BottomNavbar",
+};
+</script>
+
+<style lang="scss" scoped></style>
