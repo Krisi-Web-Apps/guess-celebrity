2af82b8109c630b6dc04ed502fdd72066f1ec600
krisikostadinov <krisi.199898@gmail.com>
2023-04-22 03:15:11 +0300

    feature: created celebrity form

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <h1 class="text-4xl text-center my-10">Познай извесната личност</h1>
+
+  <div class="container mx-auto border shadow-lg p-5">
+    <display-celebrity />
+  </div>
   <bottom-navbar />
 </template>
 
---
+++ client/src/components/CelebrityForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <div class="flex flex-col">
+      <label for="famous_name">Име</label>
+      <input
+        type="text"
+        placeholder="Въведете името, с което е известна знаменитостта..."
+        class="form-control"
+        autofocus
+      />
+    </div>
+    <div>
+      <button class="primary-btn mt-4">Готово</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CelebrityForm",
+};
+</script>
+
+<style lang="scss" scoped></style>
