2b0035f4a75474d79d4a9c9c803b33e098da4b66
krisikostadinov <krisi.199898@gmail.com>
2023-04-22 03:16:48 +0300

    feature: display celebrity design

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,13 +3,22 @@
 
   <div class="container mx-auto border shadow-lg p-5">
     <display-celebrity />
+    <celebrity-form />
   </div>
   <bottom-navbar />
 </template>
 
 <script>
+import CelebrityForm from "./components/CelebrityForm.vue";
+import DisplayCelebrity from "./components/DisplayCelebrity.vue";
+import BottomNavbar from "./components/BottomNavbar.vue";
 
 export default {
   name: "App",
+  components: {
+    CelebrityForm,
+    DisplayCelebrity,
+    BottomNavbar,
+  },
 };
 </script>
---
+++ client/src/components/DisplayCelebrity.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center">
+    <div class="w-[300px] h-[300px] rounded-full overflow-hidden">
+      <img
+        src="https://cdn.britannica.com/11/222411-050-D3D66895/American-politician-actor-athlete-Arnold-Schwarzenegger-2016.jpg"
+        class="w-full h-auto"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayCelebrity",
+};
+</script>
+
+<style lang="scss" scoped></style>
