f1c6346296796026e3b45879ed08433b527a642f
krisikostadinov <krisi.199898@gmail.com>
2023-04-26 10:52:55 +0300

    refactor: database connection is updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const config = {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "celebrities_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 }
 
 const connection = mysql.createConnection(config);
