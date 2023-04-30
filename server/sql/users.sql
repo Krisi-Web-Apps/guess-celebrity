68ca1f07117a7097bbc0b57930ffd449ecadfe08
krisikostadinov <krisi.199898@gmail.com>
2023-05-01 01:00:20 +0300

    feature: add 'role_as' field on the db

--- server/sql/users.sql
+++ server/sql/users.sql
@@ -1,5 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR (75) UNIQUE,
-    password VARCHAR (255)
+    id int(11) NOT NULL AUTO_INCREMENT,
+    email varchar(75) DEFAULT NULL,
+    password varchar(255) DEFAULT NULL,
+    role_as varchar(10) NOT NULL DEFAULT 'user',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY email ('email')
 );
